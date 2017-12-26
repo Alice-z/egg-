84c645a60a6b37b2762dad0e1ec800a155999699
alice <375898312@qq.com>
2017-12-26 20:47:59 +0800

    :car:

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  router.get('/', controller.home.index);
   router.get('/login', controller.user.login);
   router.get('/signin', controller.user.signin);
   router.get('/project/find', controller.project.find);
